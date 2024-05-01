04ea99d2586d6c9da9212325c52243f150f493d8
ewisses <wisses@gmail.com>
2024-05-01 16:30:58 -0300

    Type string

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ console.log(typeof x);
 var y = 15.5884848;
 console.log(typeof y);
 console.log(y.toPrecision(3));
+//2- string
+var firstName = "Edson";
+console.log(firstName.toUpperCase());
+var fullName;
+var lastName = "Wisses";
+fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(typeof fullName);
--- index.ts
+++ index.ts
@@ -1,16 +1,19 @@
 //1 - number
-
 let x: number = 10
-
 console.log(x)
-
 x = 16
-
 console.log(typeof x)
-
 const y:number = 15.5884848
-
 console.log(typeof y)
-
 console.log(y.toPrecision(3))
+
+//2- string
+
+const firstName: string = "Edson"
+console.log(firstName.toUpperCase())
+let fullName: string
+const lastName:string = "Wisses"
+fullName = firstName+" "+lastName
+console.log(fullName)
+console.log(typeof fullName)
 
